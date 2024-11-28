e158dd4a6ac79d651b122725e1e601d8aa9472ca
totwjt <totwjt@sina.cn>
2024-11-28 09:32:17 +0800

    init

---
+++ dist-electron/main.js
@@ -0,0 +1,27 @@
+"use strict";
+const { app, BrowserWindow } = require("electron");
+const { join } = require("path");
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "electron-vite",
+    icon: join(__dirname, "../public/logo.ico")
+  });
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(join(__dirname, "../dist/index.html"));
+  }
+};
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
